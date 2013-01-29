49afaf76e72256fca77e435461325e1055964184
yves <steve.harrison@gmx.net>
2013-01-29 16:40:33 +0100

    inital commit

---
+++ lib/minisat/core/depend.mk
@@ -0,0 +1,38 @@
+/Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.o /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.or /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.od /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.op:  \
+ /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.cc \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/System.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/Options.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/XAlloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Dimacs.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/SolverTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alg.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Map.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Heap.h
+/Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.o /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.or /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.od /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.op:  \
+ /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.cc \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Sort.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/XAlloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Heap.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alg.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/Options.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/SolverTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Map.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alloc.h
---
+++ lib/minisat/simp/depend.mk
@@ -0,0 +1,83 @@
+/Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/Main.o /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/Main.or /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/Main.od /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/Main.op:  \
+ /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/Main.cc \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/System.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/Options.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/XAlloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Dimacs.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/SolverTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alg.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Map.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/SimpSolver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Queue.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Heap.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/Options.h
+/Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/SimpSolver.o /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/SimpSolver.or /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/SimpSolver.od /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/SimpSolver.op:  \
+ /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/SimpSolver.cc \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Sort.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/XAlloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/simp/SimpSolver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Queue.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Heap.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alg.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/Options.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/SolverTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Map.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/System.h
+/Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.o /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.or /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.od /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.op:  \
+ /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Main.cc \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/System.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/Options.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/XAlloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Dimacs.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/SolverTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alg.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Map.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Heap.h
+/Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.o /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.or /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.od /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.op:  \
+ /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.cc \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Sort.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/XAlloc.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/Solver.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Heap.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alg.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/Options.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Vec.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/utils/ParseUtils.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/core/SolverTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/IntTypes.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Map.h \
+  /Users/christophbenzmuellerimac/leo/trunk/lib/minisat/mtl/Alloc.h
